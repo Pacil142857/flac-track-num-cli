b6eb89d1990a846130d57e9521171dfd76deb7fe
Pacil142857 <austinmcgregor2@gmail.com>
2021-05-29 20:50:14 -0400

    Can now add custom track numbers if the -c or --custom flag is given as a command-line argument

--- flac-track-num-cli.py
+++ flac-track-num-cli.py
@@ -15,7 +15,11 @@ def select_file(file_list, track_num):
             print(f'[{i + 1}]: {file}')
         
         # Ask user which file to pick
-        selected_file = input(f'Track #{track_num}: ')
+        selected_file = input(f'Track #{track_num} (type nothing to quit): ')
+        
+        # Quit, if the user wants to
+        if not selected_file:
+            sys.exit()
     
         try:         
             # The number is too big or below 1
@@ -30,11 +34,21 @@ def select_file(file_list, track_num):
             print('Invalid input. Please type in the number of the track you want to choose.')
 
 
+custom_track_nums = False
 # The path to the FLAC files should be given as a command-line argument
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     path = input('Path to FLAC files: ')
 else:
-    path = sys.argv[1]
+    if sys.argv[1] in ('-c', '--custom'):
+        custom_track_nums = True
+        
+        if len(sys.argv) != 3:
+            path = input('Path to FLAC files: ')
+        else:
+            path = sys.argv[2]
+    
+    else: 
+        path = sys.argv[1]
 
 # Go to the path where the FLAC files are
 try:
@@ -57,12 +71,39 @@ if not FLAC_files:
 
 for i in range(len(FLAC_files)):
     
+    # Get track number
+    if custom_track_nums:
+        while True:
+            track_num = input('Track # (type nothing to quit): ')
+            
+            # Quit if the input is empty
+            if not track_num:
+                sys.exit()
+            
+            # Validate input
+            try:
+                
+                # Ensure that the track number is a non-negative integer
+                if int(track_num) >= 0 and int(track_num) == float(track_num):
+                    track_num = int(track_num)
+                    break
+                else:
+                    print('Track number must be a non-negative integer.')
+
+            except ValueError:
+                # The user didn't input a number
+                print('Track number must be a non-negative integer.')
+    
+    else:
+        track_num = i + 1
+    
     # Get the file
-    file = select_file(FLAC_files, i + 1)
+    file = select_file(FLAC_files, track_num)
     print(file)
     FLAC_files.remove(file)
     
+    # Edit the track number
     audio = FLAC(file)
-    audio['TrackNumber'] = str(i + 1)
+    audio['TrackNumber'] = str(track_num)
     audio.save()
     
